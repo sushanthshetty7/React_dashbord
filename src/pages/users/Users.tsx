3f5f67f49c33dff4aeb15560173cf9483ae18fd7
sushanthshetty7 <sushanthr052@gmail.com>
2023-12-09 20:09:30 +0530

    user

--- src/pages/users/Users.tsx
+++ src/pages/users/Users.tsx
@@ -1,8 +1,15 @@
+import DataTabel from '../../components/Datatabel/DataTabel'
 import './users.scss'
 
 const Users=()=> {
   return (
-    <div className='users'>Users</div>
+    <div className='users'>
+      <div className="info">
+        <h1>Users</h1>
+        <button>Add New User</button>
+      </div>
+      <DataTabel/>
+    </div>
   )
 }
 
